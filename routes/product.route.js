82da2493057dc6c6ac84c3e224db1f8c90e9cb5c
Rahul <rahuldevan2019@gmail.com>
2024-02-20 17:44:10 +0530

    added some comments for better understanding

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,14 +2,19 @@ const express=require("express")
 const router=express.Router();
 const {getProducts,getProduct, createProduct, updatedProduct, deleteProduct}=require('../controllers/product.controller.js')
 
+// displaying all products
 router.get('/',getProducts)
 
+// displaying single product
 router.get('/:id',getProduct)
 
+// adding new product
 router.post('/',createProduct)
 
+// updating a product
 router.put('/:id',updatedProduct);
 
+// deleting a product
 router.delete('/:id',deleteProduct);
 
 
